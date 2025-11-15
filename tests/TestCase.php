b6ac57f97c99cdafd8a17c18c52f857982042259
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-11-15 04:24:59 +0300

    init project

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aristonis\LaravelLivewireDataview\Tests;
+
+
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+  public function setUp(): void
+  {
+    parent::setUp();
+    // additional setup
+  }
+
+  protected function getEnvironmentSetUp($app)
+  {
+    // perform environment setup
+  }
+}
